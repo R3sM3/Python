ae6ffc65938fbf7b5aa12f91627c431d504feaea
Raúl <rsmescobar@gmail.com>
2023-03-27 12:45:12 -0300

    Commit de proyectos en Python

---
+++ Proyectos/proyecto002/src/users.py
@@ -0,0 +1,6 @@
+users = [
+    {"name": "Jon"},
+    {"name": "Peter"},
+    {"name": "Joe"},
+    {"name": "Sean"}
+]
---
+++ Proyectos/proyecto005/enviarCorreo.py
@@ -0,0 +1,33 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+mail_content = 'Este es un correo de prueba usando Python vía GMAIL'
+
+#The mail addresses and password
+
+sender_address = 'raul.san.martin.escobar@gmail.com'
+sender_pass = 'Stick.2022'
+receiver_address = 'rsmescobar@gmail.com'
+
+#Setup the MIME
+
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+
+#The body and the attachments for the mail
+
+message.attach(MIMEText(mail_content, 'plain'))
+
+#Create SMTP session for sending the mail
+
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
