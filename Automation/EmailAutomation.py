9baf7b01edf734401e690e3d4baf813944cf0d59
Raúl San Martín Escobar <45212022+R3sM3@users.noreply.github.com>
2023-05-28 12:55:10 -0400

    Update EmailAutomation.py

--- Automation/EmailAutomation.py
+++ Automation/EmailAutomation.py
@@ -1,3 +1,7 @@
+#  Este codigo fue extraido desde la siguiente pagina
+#  https://medium.com/@naseer1015922/5-killer-python-scripts-for-automation-part1-e83383740154
+#  Fue creado por https://medium.com/@naseer1015922
+
 import smtplib
 from email.mime.text import MIMEText
 
